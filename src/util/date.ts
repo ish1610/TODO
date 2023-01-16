b8d506223855ded26a1fd79c34cac2434578ffa7
ish <lshsh5656@naver.com>
2023-01-16 23:16:27 +0900

    Feat : getDate 10시, 10분 미만 시간 앞에 0 추가

--- src/util/date.ts
+++ src/util/date.ts
@@ -7,8 +7,18 @@ export const getDate = () => {
   const month = newDate.getMonth();
   const date = newDate.getDate();
   const day = newDate.getDay();
-  const hour = newDate.getHours();
-  const minute = newDate.getMinutes();
+  let hour: string | number = newDate.getHours();
+  let minute: string | number = newDate.getMinutes();
+
+  if (+minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
 
   return `${month + 1}월 ${date}일 ${week[day]}요일 ${hour} : ${minute}`;
 };
+
+getDate();
