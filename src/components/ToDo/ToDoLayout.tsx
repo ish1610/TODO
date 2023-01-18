65584272be3efe88be0f4c5acd2dec8e4e661314
ish <lshsh5656@naver.com>
2023-01-18 22:25:21 +0900

    Refator : ToDoLayout todoListApi 호출 ➡️ ToDoList toDoList API 호출

--- src/components/ToDo/ToDoLayout.tsx
+++ src/components/ToDo/ToDoLayout.tsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { ToDoAPI } from "./api/toDo";
-import { toDoAction } from "../../store/ToDo/toDoSlice";
+import { useState } from "react";
 
-import { IToDoLayoutProps, ToDo } from "./types/todos";
+import { IToDoLayoutProps } from "./types/todos";
 import ToDoLayoutView from "./Views/ToDoLayoutView";
 
 const ToDoLayout = () => {
   const [isShowModal, setIsShowModal] = useState(false);
-  const dispatch = useDispatch();
 
   const onCloseModal = () => {
     setIsShowModal(false);
@@ -19,12 +15,6 @@ const ToDoLayout = () => {
     onShowModal: () => setIsShowModal(true),
     isShowModal,
   };
-
-  useEffect(() => {
-    ToDoAPI.getToDo((toDoList: ToDo[]) =>
-      dispatch(toDoAction.getToDoList(toDoList))
-    );
-  }, []);
 
   return <ToDoLayoutView {...ToDoLayouyProps} />;
 };
--- src/components/ToDo/ToDoList.tsx
+++ src/components/ToDo/ToDoList.tsx
@@ -1,15 +1,25 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toDoAction } from "../../store/ToDo/toDoSlice";
+import { ToDoAPI } from "./api/toDo";
 
-import { IToDoListProps } from "./types/todos";
+import { IToDoListProps, ToDo } from "./types/todos";
 import ToDoListView from "./Views/ToDoListView";
 
 const ToDoList = ({ onShowModal }: IToDoListProps) => {
+  const dispatch = useDispatch();
   const toDoList = useSelector((state: any) => state.toDoList.toDoList);
 
   const toDoListProps = {
     onShowModal,
     toDoList,
   };
+
+  useEffect(() => {
+    ToDoAPI.getToDo((toDoList: ToDo[]) =>
+      dispatch(toDoAction.getToDoList(toDoList))
+    );
+  }, []);
 
   return <ToDoListView {...toDoListProps} />;
 };
