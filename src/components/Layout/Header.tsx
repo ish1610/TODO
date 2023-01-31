2d2e2d5a17149204c78b711777c67bb2e7a728e3
ish <lshsh5656@naver.com>
2023-01-31 22:12:41 +0900

    Header import 정리

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { loginAction } from "../../store/Auth/loginSlice";
