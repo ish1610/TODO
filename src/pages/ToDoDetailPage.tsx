0aeeb8de780ac94aef81c57e02cb9e517c9103fe
ish <lshsh5656@naver.com>
2023-01-31 22:15:26 +0900

    ToDoDetailPage import 정리

--- src/pages/ToDoDetailPage.tsx
+++ src/pages/ToDoDetailPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import ToDoDetail from "../components/ToDoDetail/ToDoDetail";
 import ToDoDetailLayout from "../components/ToDoDetail/ToDoDetailLayout";
 
 const ToDoDetailPage = () => {
