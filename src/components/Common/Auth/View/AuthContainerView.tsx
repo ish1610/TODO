009029b46c8b83e4628e88905f886206a74ac7a1
ish <lshsh5656@naver.com>
2023-02-01 17:16:08 +0900

    Design : AuthContainerView 최소 높이 수정

--- src/components/Common/Auth/View/AuthContainerView.tsx
+++ src/components/Common/Auth/View/AuthContainerView.tsx
@@ -5,8 +5,8 @@ import { IAuthContainerProps } from "../types/auth";
 const Container = styled.div`
   position: relative;
   height: calc(100vh - 120px);
-  min-height: 750px;
-  padding: 120px 36px 180px;
+  min-height: 700px;
+  padding: 80px 36px 180px;
   min-width: 450px;
   display: flex;
   align-items: center;
