e606d753aa0e9e1b1d6b4b805812cc00dd3dc8c1
ish <lshsh5656@naver.com>
2023-02-01 19:54:10 +0900

    Refactor : ToDoList - disPatchGetToDoList useCallback

--- src/components/ToDo/ToDoList.tsx
+++ src/components/ToDo/ToDoList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toDoAction } from "../../store/ToDo/toDoSlice";
 import { Store } from "../../store/types/store";
@@ -16,13 +16,16 @@ const ToDoList = ({ onShowModal }: IToDoListProps) => {
     toDoList,
   };
 
-  const disPatchGetToDoList = (toDoList: ToDo[]) => {
-    dispatch(toDoAction.getToDoList(toDoList));
-  };
+  const disPatchGetToDoList = useCallback(
+    (toDoList: ToDo[]) => {
+      dispatch(toDoAction.getToDoList(toDoList));
+    },
+    [dispatch]
+  );
 
   useEffect(() => {
     ToDoAPI.getToDo((toDoList: ToDo[]) => disPatchGetToDoList(toDoList));
-  }, []);
+  }, [disPatchGetToDoList]);
 
   return <ToDoListView {...toDoListProps} />;
 };
